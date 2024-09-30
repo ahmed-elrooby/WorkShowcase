d91d9d6e0043b179bad9ce24755bd2905ecf9ca4
Ahmed_Eid <99208677+ahmed-elrooby@users.noreply.github.com>
2024-09-30 21:03:10 +0300

    Update Up.jsx

--- src/Component/Up/Up.jsx
+++ src/Component/Up/Up.jsx
@@ -1,35 +1,4 @@
-// "use client"
-// import { ArrowUpFromDot } from "lucide-react";
-// import React, { useState } from "react";
 
-// const Up = () => {
-//   const [show, setShow] = useState(false);
-//   window.addEventListener("scroll", () => {
-//     window.scrollY > 400 ? setShow(true) : setShow(false);
-//   });
-//   const handleTop = () => {
-//     window.scrollTo({
-//       top: 0,
-//       behavior: "smooth",
-//     });
-//   };
-//   return (
-//     <>
-//       {show ? (
-//         <div
-//           onClick={handleTop}
-//           className="flex animate-bounce  fixed bottom-[100px] right-[20px] items-center justify-center w-12 h-12 font-bold text-[--main-color] z-[5555] transition-all border-2 border-[--main-color] rounded-full cursor-pointer hover:bg-[--main-color] hover:text-white"
-//         >
-//           <ArrowUpFromDot />
-//         </div>
-//       ) : (
-//         ""
-//       )}
-//     </>
-//   );
-// };
-
-// export default Up;
 "use client"
 import { useState, useEffect } from "react";
 
